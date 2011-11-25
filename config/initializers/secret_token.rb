978a0b18111dbe7de2935c1d6922864cfa293d29
Nima Izadi <nim.izadi@gmail.com>
2011-11-25 09:47:52 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuit2011::Application.config.secret_token = '37f270e278fb229d8ab1b9dd5fb5e6d8ee478560129363373c240e48cc1c1cc434777789256951eb71a91184ac3f1d3eb250e96a576503ff8760d022a8878341'
