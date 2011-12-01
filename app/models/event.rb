15e4f4bb62c94c2bf73e6a1f48de68bf91fb122d
Nima Izadi <nim.izadi@gmail.com>
2011-12-01 21:59:41 +0100

    Add budget gestion

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :creator, :class_name => "User"
+  
+  def budget
+    Budget.where(:event_id => 1).collect{|b| b.amount}.inject(0) { |s,v| s += v }
+  end
 end
